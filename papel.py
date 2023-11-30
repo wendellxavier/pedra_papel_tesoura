d473cff9543966dc6afb918b77493ad341f9cf94
wendell <wendell_xw20@hotmail.com>
2023-11-30 16:15:05 -0300

    comentario

---
+++ papel.py
@@ -0,0 +1,69 @@
+import random
+
+def valida_int(pergunta, min, max):
+    x = int(input(pergunta))
+    while x < min or x > max:
+        x = int(input(pergunta))
+    return x
+
+def vencedor(jogador1, jogador2):
+    global empate, v1, v2
+    
+    if jogador1 == 1:  # pedra
+        if jogador2 == 1:
+            empate += 1
+        elif jogador2 == 2:
+            v2 += 1
+        elif jogador2 == 3:
+            v1 += 1
+    elif jogador1 == 2:  # papel
+        if jogador2 == 1:
+            v1 += 1
+        elif jogador2 == 2:
+            empate += 1
+        elif jogador2 == 3:
+            v2 += 1
+    elif jogador1 == 3:  # tesoura
+        if jogador2 == 1:
+            v2 += 1
+        elif jogador2 == 2:
+            v1 += 1
+        elif jogador2 == 3:
+            empate += 1
+    
+    resultados = [v1, v2, empate]
+    return resultados
+
+print("JOKENPO")
+print("1- pedra")
+print("2- papel")
+print("3- tesoura")
+print("para sair do jogo aperta letra "n" ")
+
+resultados = []
+jogadas = []
+
+v1 = 0
+v2 = 0
+empate = 0
+
+loop = True
+while loop:
+    j1 = valida_int("Escolha sua jogada: ", 1, 3)
+    if not j1:
+        break
+    j2 = random.randint(1, 3)
+    jogadas.append([j1, j2])
+    resultados = vencedor(j1, j2)
+
+    for jogada in jogadas:
+        for dado in jogada:
+            print(dado, end=' ')
+        print()
+    respota = input('Deseja continuar(s/n): ')
+    if respota.lower() != "s":
+        loop = False
+
+print("Número de vitórias do jogador 1: {}".format(resultados[0]))
+print("Número de vitórias do jogador 2: {}".format(resultados[1]))
+print("Número de empates: {}".format(resultados[2]))
